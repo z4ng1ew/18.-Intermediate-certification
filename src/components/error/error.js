9c35fdf44d0a7bf39037b0707af92730e555fdf7
z4ng1ew <z4ng1ew@gmail.com>
2024-01-13 23:35:36 +0300

    ервый коммит

---
+++ src/components/error/error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error404 = () => {
+  return (
+    <div>
+      <h1>Ошибка 404: Страница не найдена</h1>
+      <p>Извините, запрошенная вами страница не существует.</p>
+    </div>
+  );
+};
+
+export default Error404;
